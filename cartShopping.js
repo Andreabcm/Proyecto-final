8431b96c2f1af8b296e547d02d3722a1552c1e72
MAlexGG <alexandragalarzag@gmail.com>
2020-12-10 10:58:43 +0100

    versionAlex

--- cartShopping.js
+++ cartShopping.js
@@ -1,3 +1,26 @@
-function cart () {
-    
+function cleanBody() {
+    let principal = document.getElementById("principal");
+        if (principal.style.display === "block") {
+            principal.style.display = "none";
+        } else {
+            principal.style.display = "block";
+        }
 }
+
+
+function cart() {
+
+    this.cleanBody();
+    
+};
+
+
+
+
+
+/* principal.style.display = (principal.style.display === "block")?"none":"block" 
+
+
+    condicionales ternarios
+*/   
+    
