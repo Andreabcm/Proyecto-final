eb7a9d32ec2cd71ac2882c6cfb431c4562be1976
Andreabcm <andreabcm16@gmail.com>
2020-12-10 15:21:30 +0100

    versionAndreaJava

--- cartShopping.js
+++ cartShopping.js
@@ -1,14 +1,21 @@
 function cleanBody() {
     let principal = document.getElementById("principal");
     if (principal.style.display === "block") {
-        principal.style.display = "none";
+        principal.style.display = "none";  
+    }
+    else {
+        principal.style.display = "block"
     }
 };
+
 
 function paginaSegunda() {
     let checkout = document.getElementById("checkout");
     if (checkout.style.display === "none") {
         checkout.style.display = "block";
+    }
+    else {
+        checkout.style.display = "none"
     }
 };
 
