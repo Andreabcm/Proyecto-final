6b599180881cf499fc0c3d3a7688215f5d67d0d2
pachorin <scorcuera6@gmail.com>
2020-12-11 15:37:40 +0100

    versionAlex5

--- scripts/categorias.js
+++ scripts/categorias.js
@@ -1,26 +1,59 @@
-let categorias = [
-    {
-        imgCategoria: ".img/slider/breakfast.svg",
-        nombre: "Desayunos",
-    }
+let listaCategorias = [
 
     {
-        imgCategoria: ".img/slider/breakfast.svg",
-        nombre: "Almuerzos",
-    }
+        imgCategoria: "./img/slider/tapas.svg",
+        nombreCategoria: "Tapas",
+    },
 
     {
-        imgCategoria: ".img/slider/breakfast.svg",
-        nombre: "Ensaladas",
-    }
+        imgCategoria: "./img/slider/breakfast.svg",
+        nombreCategoria: "Desayunos",
+    },
 
     {
-        imgCategoria: ".img/slider/breakfast.svg",
-        nombre: "Desayunos",
-    }
+        imgCategoria: "./img/slider/salad.png",
+        nombreCategoria: "Ensaladas",
+    },
 
     {
-        imgCategoria: ".img/slider/breakfast.svg",
-        nombre: "Desayunos",
+        imgCategoria: "./img/slider/fastfood.svg",
+        nombreCategoria: "Almuerzos",
+    },
+
+    {
+        imgCategoria: "./img/slider/dinner.svg",
+        nombreCategoria: "Cena",
+    },
+
+    {
+        imgCategoria: "./img/slider/desert.svg",
+        nombreCategoria: "Postres",
+    },
+
+    {
+        imgCategoria: "./img/slider/bebidas.svg",
+        nombreCategoria: "Bebidas",
+    },
+
+    {
+        imgCategoria: "./img/slider/all.svg",
+        nombreCategoria: "Todos",
+    },
+];
+
+
+
+let cardSlider = "";
+
+function categoriasImprimir() {
+    for (let i = 0; i < listaCategorias.length; i++) {
+        cardSlider += `<div class="cardSlider">
+        <img src="${listaCategorias[i].imgCategoria}" alt=" ">
+        <p>${listaCategorias[i].nombreCategoria}</p>
+        </div>`
     }
-]
+}
+
+categoriasImprimir();
+document.querySelector(".slider").innerHTML = cardSlider;
+
