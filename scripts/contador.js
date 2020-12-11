556bbe33226c9e02b8ff18eb55a2a4f5dacb270b
PPIICCAA <AbdulhaIdrees@gmail.com>
2020-12-11 13:51:52 +0100

    Contador Nuevo

--- scripts/contador.js
+++ scripts/contador.js
@@ -1,14 +1,13 @@
 function suma(objeto) {
-   let indicearray = objeto.parentElement.dataset.numero;
-   let cantidadItems = listaPlatos[indicearray].cantidad;
-        cantidadItems++
-        console.log(listaPlatos[indicearray].cantidad = cantidadItems);
-}
+  let indicearray = objeto.parentElement.dataset.numero;
+  listaPlatos[indicearray].cantidad++
+  objeto.parentElement.querySelector("p").innerHTML = listaPlatos[indicearray].cantidad;
+} 
 
 function resta(objeto) {
-  let i = parseFloat(objeto.parentElement.querySelector("inpugitt").value)
-    if(i > 0) {
-        i--;
-        return objeto.parentElement.querySelector("input").value = i;
-    }
-}
+  let indicearray = objeto.parentElement.dataset.numero;
+    if (listaPlatos[indicearray].cantidad > 0) {
+    listaPlatos[indicearray].cantidad--
+    objeto.parentElement.querySelector("p").innerHTML = listaPlatos[indicearray].cantidad;
+  }
+} 
