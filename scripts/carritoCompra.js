510ca64596e7667c0b2ed29cf9345a454b74459a
PPIICCAA <AbdulhaIdrees@gmail.com>
2020-12-14 15:02:02 +0100

    Porfin

--- scripts/carritoCompra.js
+++ scripts/carritoCompra.js
@@ -1,22 +1,20 @@
 function imprimirCarrito() {
-    let texto = ""
-    
+    let texto = "";
+    console.log(carritoDeCompra);
 for (let i = 0; i < carritoDeCompra.length; i++) {
     texto += `<div class="elemento">
-    <img src="${secondArray[i].imgPLato}" alt=" ">
+    <img src="${carritoDeCompra[i].imgPLato}" alt=" ">
     <div class="texto ">
-        <p>${secondArray[i].precioPlato}&#8364</p>
-        <h5>${secondArray[i].nombrePlato}</h5>
-        <h6>${secondArray[i].descripcionPlato}</h6>
+        <p>${carritoDeCompra[i].precioPlato}&#8364</p>
+        <h5>${carritoDeCompra[i].nombrePlato}</h5>
+        <h6>${carritoDeCompra[i].descripcionPlato}</h6>
     </div>
     <div class="contador" data-numero="${[i]}">
         <button onClick="resta(this)" class="resta">-</button>
-        <p class="quantity">${secondArray[i].cantidad}</p>
+        <p class="quantity">${carritoDeCompra[i].cantidad}</p>
         <button onClick="suma(this)" class="suma">+</button>
     </div>
 </div>`
     }
-    
-    document.querySelector(".elementosMenu2").innerHTML = secondArray;
-    console.log(secondArray)
+    document.querySelector(".elementosMenu2").innerHTML = texto;
 }
