f33b983f6eaf943e07e1fde136aab84f5582cf9d
SIMOKLK <alexsimoesfernandez@gmail.com>
2020-12-16 15:03:01 +0100

    id

--- scripts/contador.js
+++ scripts/contador.js
@@ -1,40 +1,54 @@
+function getPlato(arrayMenu, idPlato) {
+    let plato = arrayMenu.find(function(item) {
+        return item.id == idPlato;
+    });
+    return plato;
+}
+
 function suma(objeto) {
-  let indicearray = objeto.parentElement.dataset.numero;
-  listaPlatos[indicearray].cantidad++
-  objeto.parentElement.querySelector("p").innerHTML = listaPlatos[indicearray].cantidad;
-  
-  addToCart(listaPlatos[indicearray])
-  } 
+    let idPlato = objeto.parentElement.dataset.numero;
+    let objetoPlato = getPlato(listaPlatos, idPlato);
+    objetoPlato.cantidad++;
+    addToCart(objetoPlato);
+    objeto.parentElement.querySelector("p").innerHTML = objetoPlato.cantidad;
+
+}
 
 function resta(objeto) {
-  let indicearray = objeto.parentElement.dataset.numero;
+    let indicearray = objeto.parentElement.dataset.numero;
     if (listaPlatos[indicearray].cantidad > 0) {
-    listaPlatos[indicearray].cantidad--
-    objeto.parentElement.querySelector("p").innerHTML = listaPlatos[indicearray].cantidad;
+        listaPlatos[indicearray].cantidad--
+            objeto.parentElement.querySelector("p").innerHTML = listaPlatos[indicearray].cantidad;
 
-    borrarItem(listaPlatos[indicearray])
-  }
-} 
+        borrarItem(listaPlatos[indicearray])
+    }
+}
 
 function addToCart(itemPlato) {
-  if(itemPlato.cantidad == 1){
-          carritoDeCompra.push(itemPlato)
-        }
-  
+    let idPlato = itemPlato.id;
+    let platoEnCarrito = getPlato(carritoDeCompra, idPlato);
+
+    if (platoEnCarrito === undefined) {
+        carritoDeCompra.push(itemPlato);
+    }
+    return;
+
+
+
 }
 
 function borrarItem(itemPlato) {
-  let indice = carritoDeCompra.indexOf(itemPlato);
-  if (itemPlato.cantidad == 0){
-    carritoDeCompra.splice(indice,1);
-  }
+    let indice = carritoDeCompra.indexOf(itemPlato);
+    if (itemPlato.cantidad == 0) {
+        carritoDeCompra.splice(indice, 1);
+    }
 }
 
 function eliminarItem(botonEliminar) {
-  let indiceCarrito = botonEliminar.parentElement.dataset.numero;
-  carritoDeCompra[indiceCarrito].cantidad = 0;
-  carritoDeCompra.splice(indiceCarrito,1);
-  if (carritoDeCompra[indiceCarrito].cantidad === 0){
-    document.querySelector(".quantity").innerHTML = 0;
-  }
+    let indiceCarrito = botonEliminar.parentElement.dataset.numero;
+    carritoDeCompra[indiceCarrito].cantidad = 0;
+    carritoDeCompra.splice(indiceCarrito, 1);
+    if (carritoDeCompra[indiceCarrito].cantidad === 0) {
+        document.querySelector(".quantity").innerHTML = 0;
+    }
 }
