06675713f7e2a21c067edd7ca8491d71b2e4eea3
Andreabcm <andreabcm16@gmail.com>
2020-12-17 15:14:31 +0100

    botonEnviarPedido

--- scripts/desglosePrecio.js
+++ scripts/desglosePrecio.js
@@ -1,22 +1,33 @@
+const numeroPropietario = 34643423177;
+let pedidoFinal = "Pedido:";
+let totalFactura = 0;
+
 function imprimirPrecio() {
-      let subtotal = 0;
-      
-      
-      for(i=0; i<carritoDeCompra.length; i++){
+      let subtotal = 0;      
+      for(let i=0; i<carritoDeCompra.length; i++){
             let precioItem = carritoDeCompra[i].precioPlato * carritoDeCompra[i].cantidad;
             subtotal += precioItem; 
       }
 
       let tasas = 0.10;
       let totalTasas = subtotal * tasas;
-      let total = subtotal + totalTasas;
-     
-
-
+      totalFactura = subtotal + totalTasas;
       document.getElementById("subtotal").innerHTML = subtotal + "&#8364";
       document.getElementById("tasas").innerHTML = totalTasas.toPrecision(2) + "&#8364";
       document.getElementById("delivery").innerHTML = "FREE";
-      document.getElementById("total").innerHTML = total + "&#8364";
+      document.getElementById("total").innerHTML = totalFactura + "&#8364";
+
    }
 
-  
+  function botonEnviarPedido(){
+      messaggeBuilder();
+      window.location.href ="https://api.whatsapp.com/send/?phone=" + numeroPropietario + "&text="+ pedidoFinal +"&app_absent=0";
+}
+
+function messaggeBuilder() {
+      for (let i = 0; i < carritoDeCompra.length; i++){
+            let textopedido = carritoDeCompra[i].nombrePlato +"x(" + carritoDeCompra[i].cantidad + ") --"
+            pedidoFinal += textopedido;
+      }
+      pedidoFinal += "Total de factura" + totalFactura;
+}
